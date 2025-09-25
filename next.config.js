ad74984c4925888cb26058e53fa562178e9c54cd
abhijith0070 <160753563+abhijith0070@users.noreply.github.com>
2025-09-25 17:15:59 +0530

    Your

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  onDemandEntries: {
+    // Suppress dynamic server usage warnings during build
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  }
 }
 
 module.exports = nextConfig
